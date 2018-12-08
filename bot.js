e647571d0f240088ccc6ce7df862b9db0ca25ad2
kubulll <43418203+kubulll@users.noreply.github.com>
2018-12-08 08:12:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,19 @@ client.on('ready', () => { // know if our bot is online
 
 client.on('message', message => { // When the message a message is executed
 
-    if (message.content === 'kys') { // if that message matches ping
+    if (message.content === 'pytong') { // if that message matches ping
 
        message.reply('kys kys kys'); // 
      var role = message.guild.roles.find(role => role.name === "miętka faja");
+     
      message.member.addRole(role);
 
+       }
+       if(message.content.includes('kys')) {
+        
+           message.reply('kys kys kys');  
+           var role = message.guild.roles.find(role => role.name === "miętka faja");
+        
        }
 
 });
